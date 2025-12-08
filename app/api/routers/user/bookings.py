70fd42c954d22eaac84f8d16517b51992270506a
Jmodhiambo <jmodhiambo9346@gmail.com>
2025-12-08 14:32:51 +0300

    Update payment route

--- app/api/routers/user/bookings.py
+++ app/api/routers/user/bookings.py
@@ -9,7 +9,7 @@ import app.services.booking_services as booking_services
 
 router = APIRouter()
 
-@router.post("/users/{user_id}/bookings", response_model=list[BookingOut])
+@router.post("/users/{user_id}/bookings", response_model=list[BookingOut], status_code=status.HTTP_201_CREATED)
 async def create_bookings(user_id: int, booking: BookingCreate, user=Depends(get_current_user)):
     """
     Create a new booking.
@@ -19,7 +19,7 @@ async def create_bookings(user_id: int, booking: BookingCreate, user=Depends(get
     return booking_services.create_booking_service(booking)
 
 
-@router.get("/users/{user_id}/bookings", response_model=list[BookingOut])
+@router.get("/users/{user_id}/bookings", response_model=list[BookingOut], status_code=status.HTTP_200_OK)
 async def list_bookings(user_id: int, user=Depends(get_current_user)):
     """
     List all bookings for a specific user.
@@ -28,8 +28,8 @@ async def list_bookings(user_id: int, user=Depends(get_current_user)):
         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Not authorized to view these bookings")
     return booking_services.list_bookings_by_user_service(user_id)
 
-@router.get("/users/{user_id}/bookings/{booking_id}", response_model=BookingOut)
-async def get_booking(user_id: int, booking_id: int, user=Depends(get_current_user)):
+@router.get("/users/{user_id}/bookings/{booking_id}", response_model=BookingOut, status_code=status.HTTP_200_OK)
+async def get_booking_by_id(user_id: int, booking_id: int, user=Depends(get_current_user)):
     """
     Get a specific booking by its ID.
     """
@@ -37,8 +37,8 @@ async def get_booking(user_id: int, booking_id: int, user=Depends(get_current_us
         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Not authorized to view this booking")
     return booking_services.get_booking_by_id_service(booking_id)
 
-@router.put("/users/{user_id}/bookings/{booking_id}", response_model=BookingOut)
-async def update_booking(user_id: int, booking_id: int, booking: BookingUpdate, user=Depends(get_current_user)):
+@router.put("/users/{user_id}/bookings/{booking_id}", response_model=BookingOut, status_code=status.HTTP_200_OK)
+async def update_booking_by_id(user_id: int, booking_id: int, booking: BookingUpdate, user=Depends(get_current_user)):
     """
     Update an existing booking.
     """
@@ -47,7 +47,7 @@ async def update_booking(user_id: int, booking_id: int, booking: BookingUpdate, 
     return booking_services.update_booking_service(booking_id, booking)
 
 @router.patch("/users/{user_id}/bookings/{booking_id}", status_code=status.HTTP_204_NO_CONTENT)
-async def cancel_booking(user_id: int, booking_id: int, user=Depends(get_current_user)):
+async def cancel_booking_by_id(user_id: int, booking_id: int, user=Depends(get_current_user)):
     """
     Delete (Mark as Cancelled) a specific booking by its ID.
     """
@@ -55,16 +55,16 @@ async def cancel_booking(user_id: int, booking_id: int, user=Depends(get_current
         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Not authorized to cancel this booking")
     booking_services.update_booking_status_service(booking_id, "Cancelled")
 
-@router.get("/users/{user_id}/bookings/status/{status}", response_model=list[BookingOut])
-async def list_bookings_by_status(user_id: int, status: str, user=Depends(get_current_user)):
+@router.get("/users/{user_id}/bookings/status/{booking_status}", response_model=list[BookingOut], status_code=status.HTTP_200_OK)
+async def list_bookings_by_status(user_id: int, booking_status: str, user=Depends(get_current_user)):
     """
     List all bookings with a specific status for a specific user.
     """
     if user.id != user_id:
         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Not authorized to view these bookings")
-    return booking_services.list_bookings_status_by_user_service(user_id, status)
+    return booking_services.list_bookings_status_by_user_service(user_id, booking_status)
 
-@router.get("/users/{user_id}/bookings/count", response_model=int)
+@router.get("/users/{user_id}/bookings/count", response_model=int, status_code=status.HTTP_200_OK)
 async def get_total_bookings(user_id: int, user=Depends(get_current_user)):
     """
     Get the total number of bookings for a specific user.
